b265951540052e51a0f03b20dfb806d2f37df6bb
JM <manzur-1990@hotmail.com>
2020-04-09 14:48:59 -0500

    finished Article.js and Menu.js

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -20,11 +20,11 @@ const data = [
     thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
-        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`,
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -40,11 +40,11 @@ const data = [
     thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
         Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
         roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
-        sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
+        sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`,
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -68,11 +68,11 @@ const data = [
 
     thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
         Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
-        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
+        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`,
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -84,8 +84,18 @@ const data = [
 
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
-          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
+  },
+  {
+    title: "Professional Software Development in 2020",
+    date: "Jan 1st, 2020",
+    firstParagraph: `House Tarly of Horn Hill The battle of the redgrass field. The Dothraki do things in their own time, for their own reasons. The rains of castamere. House Tarly of Horn Hill `,
+
+    secondParagraph: `The Dothraki do things in their own time, for their own reasons. I would like a trial by combat. It's ten thousand miles between Kings landing and the wall. The Dothraki do things in their own time, for their own reasons. A forked purple lightning bolt, on black field speckled with four-pointed stars.`,
+
+    thirdParagraph: `I would like a trial by combat. The rains of castamere. The bear and the maiden fair. It is rare to meet a Lannister who shares my enthusiasm for dead Lannisters. Fire and blood.`,
+  },
+
 ];
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
@@ -98,6 +108,7 @@ const data = [
 
     <span class='expandButton'></span>
   </div>
+
 
   Hint: You will need to use createElement more than once here!
 
@@ -112,3 +123,63 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+
+const articleBuilder = (
+  title,
+  date,
+  firstParagraph,
+  secondParagraph,
+  thirdParagraph 
+) => {
+
+  //Creating Elements
+  const article = document.createElement('div')
+  const articleTitle = document.createElement('h2')
+  const articleDate = document.createElement('p')
+  const firstP = document.createElement('p')
+  const secondP = document.createElement('p')
+  const thirdP = document.createElement('p')
+  const btnArticle = document.createElement('p')
+
+  // HTML Sturcture
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate)
+  article.appendChild(firstP);
+  article.appendChild(secondP)
+  article.appendChild(thirdP);
+  article.appendChild(btnArticle)
+
+  // Add Classes 
+  article.classList.add('article')
+  articleDate.classList.add('date')
+  btnArticle.classList.add('expandButton')
+
+  // Add Content
+  articleTitle.textContent = title
+  articleDate.textContent = date
+  firstP.textContent = firstParagraph
+  secondP.textContent = secondParagraph
+  thirdP.textContent = thirdParagraph
+  btnArticle.textContent = 'View More'
+
+  btnArticle.addEventListener('click', event => 
+    article.classList.toggle('article-open'))
+
+  return article;
+
+};
+
+// Map
+const articleContent = document.querySelector('.articles')
+  data.forEach(content => {
+    articleContent.appendChild(
+      articleBuilder(
+        content.title,
+        content.date,
+        content.firstParagraph,
+        content.secondParagraph,
+        content.thirdParagraph
+      )
+    )
+  })
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,27 +9,54 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+ 
 
-  Step 1: Write a function that will create a menu component as seen below:
+  //Step 1: Write a function that will create a menu component as seen below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as a list item}
+  //   </ul>
+  // </div>
 
-  The function takes an array as its only argument.
-
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
   
-*/
+  //The function takes an array as its only argument.
+
+  // Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+  // Add those items to the <ul>
+  
+
+  // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+
+
+  // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  
+  // Step 5: return the menu component.
+
+  
+  // Step 6: add the menu component to the DOM.
+  
+// creating a function that will create "li" items:
+function createMenu(navArray) {
+  //creating the elements:
+  const menu = document.createElement("div");
+  const menuList = document.createElement("ul");
+//adding the styles to the div:
+  menu.classList.add("menu");
+//going through each menu list item and create "li" elements:
+  navArray.forEach(item => {
+    let menuListItem = document.createElement("li");
+    menuListItem.textContent = item;
+    menuList.appendChild(menuListItem);
+  });
+//adding a click event listener that toggles on/off using 'menu--open' class: 
+  menu.appendChild(menuList);
+  document.querySelector(".menu-button").addEventListener("click", function() {
+    // console.log("clicktest");
+    menu.classList.toggle("menu--open");
+  });
+  // return the menu component:
+  return menu;
+}
+//adding components to the DOM: 
+document.querySelector(".header").append(createMenu(menuItems));
